ffe3874077c81956ee241814f095d624c4379cf3
Nicolas Garzon Vasquez <43526116+nicolasgarzon15@users.noreply.github.com>
2024-11-02 12:18:39 -0500

    Update fibonacci.tf

--- fibonacci.tf
+++ fibonacci.tf
@@ -1,30 +1,15 @@
-hcl
-Copiar código
 provider "null" {}
 
 locals {
+  # Generar la serie de Fibonacci hasta el número 21
   fibonacci = [0, 1]
+
+  # Generar los números de Fibonacci
+  count = 19
+  fib_seq = [for i in range(2, local.count + 2) : local.fibonacci[i - 2] + local.fibonacci[i - 1]]
 }
 
-# Generar la serie de Fibonacci hasta el número 21
-resource "null_resource" "fibonacci_series" {
-  count = 19 # Para generar hasta el número 21, necesitamos 19 iteraciones adicionales
-
-  provisioner "local-exec" {
-    command = <<EOT
-      last=${local.fibonacci[count.index]}
-      current=${local.fibonacci[count.index - 1]}
-      new_number=$((last + current))
-      echo $new_number >> fibonacci.txt
-    EOT
-  }
-
-  # Añadir el nuevo número a la serie
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
+# Unir los dos primeros elementos de la serie con los generados
 output "fibonacci_series" {
-  value = [for i in range(0, 21) : local.fibonacci[i]]
+  value = local.fibonacci ++ local.fib_seq
 }
