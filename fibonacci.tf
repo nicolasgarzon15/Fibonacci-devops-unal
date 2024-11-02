c515651fec5f52194a4e7c271887d7b8c42d2755
Nicolas Garzon Vasquez <43526116+nicolasgarzon15@users.noreply.github.com>
2024-11-02 12:24:15 -0500

    Update fibonacci.tf

--- fibonacci.tf
+++ fibonacci.tf
@@ -1,15 +1,5 @@
-provider "null" {}
-
-locals {
-  # Generar la serie de Fibonacci hasta el número 21
-  fibonacci = [0, 1]
-
-  # Generar los números de Fibonacci
-  count = 19
-  fib_seq = [for i in range(2, local.count + 2) : local.fibonacci[i - 2] + local.fibonacci[i - 1]]
-}
-
-# Unir los dos primeros elementos de la serie con los generados
-output "fibonacci_series" {
-  value = local.fibonacci ++ local.fib_seq
+resource "null_resource" "fibonacci" {
+  provisioner "local-exec" {
+    command = "python fibonacci.py"
+  }
 }
